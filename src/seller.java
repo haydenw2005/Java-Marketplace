97560e5bf89d1ca63fafacee9c5390fc4d3200d6
haydenswhite <hwhite@oneshot.ai>
2023-11-05 17:32:17 -0500

    small changes

--- src/seller.java
+++ src/seller.java
@@ -1,46 +1,15 @@
-import java.util.Scanner;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Seller {
-    private static Scanner scanner = new Scanner(System.in);
+import java.util.Map;
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Seller Portal!");
+public class Seller extends Person {
 
-        // Simulate login
-        if (!loginToPortal()) {
-            System.out.println("Login failed. Exiting the application.");
-            return;
-        }
-
-        // Navigate to Store A
-        navigateToStore("A");
-
-        // Add items to sell
-        addItemToSell();
-
-        System.out.println("Thank you for using the Seller Portal!");
-    }
-
-    private static boolean loginToPortal() {
-        System.out.print("Enter your username: ");
-        String username = scanner.nextLine();
-        System.out.print("Enter your password: ");
-        String password = scanner.nextLine();
-
-        return true;
-    }
-
-    private static void navigateToStore(String storeName) {
-    output that we have navigated to the store.
-        System.out.println("Navigated to Store " + storeName + ".");
-    }
-
-    private static void addItemToSell() {
-        System.out.print("Enter the name of the item you want to sell: ");
-        String itemName = scanner.nextLine();
-        System.out.print("Enter the price of the item: ");
-        String itemPrice = scanner.nextLine();
-
-        System.out.println("Added " + itemName + " for sale at $" + itemPrice + ".");
+    private Map<String, Store> stores;
+    public Seller() {}
+    public Seller(@JsonProperty("username") String username, @JsonProperty("password") String password,
+                  @JsonProperty("firstName") String firstName, @JsonProperty("lastName") String lastName,
+                  @JsonProperty("stores") Map<String, Store> stores) {
+        super(username, password, firstName, lastName);
+        this.stores = stores;
     }
 }
