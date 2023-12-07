5f1a2aaed109e75d89bfc7d51b5f591330005882
haydenswhite <hwhite@oneshot.ai>
2023-12-07 17:08:15 -0500

    concurrent solution

--- src/Server.java
+++ src/Server.java
@@ -14,77 +14,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Server {
     public static void main(String[] args) {
-        
-        ObjectMapper objectMapper = new ObjectMapper();
+        int portNumber = 4242;
+
 
         try {
-            ServerSocket serverSocket = new ServerSocket(4242);
-            
-            System.out.println("Waiting for the client to connect...");
-            Socket socket = serverSocket.accept();
-            System.out.println("Client connected!");
-            
-            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-            oos.flush();
-            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-
-            // send marketplace to client
-            Marketplace marketplace = JsonUtils.objectByKey(objectMapper, "", Marketplace.class);
-            oos.writeObject(marketplace);
-            oos.flush();
-            System.out.println("Marketplace sent to client!");
-            
-            Person user = (Person) ois.readObject(); // receive person object from client
-            Server server = new Server();
-
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            System.out.println("Listeing on port: "  + portNumber);
             while (true) {
-                String command = (String) ois.readObject();
-                System.out.println("Client Requested " + command);
-                if (command.equals("buyItem")) {
-                    Item selectedItem = (Item) ois.readObject();  // get parameters required from buyitem from user
-                    String numItems = (String) ois.readObject();  // get parameters required from buyitem from user
-                    ((Buyer) user).buyItem(selectedItem, marketplace, objectMapper, numItems);
-                    marketplace = server.getUpdatedMarketPlace(objectMapper);
-                    oos.writeObject(marketplace);// update client with new marketplace object
-                    oos.flush();
-                }
-                if (command.equals("updateMarketplace")) {
-                    marketplace = server.getUpdatedMarketPlace(objectMapper);
-                    oos.writeObject(marketplace);
-                    oos.flush();
-                }
-                if (command.equals("updateBuyer")) {
-                    user = server.getUpdatedBuyer(user.getUsername(), objectMapper);
-                    oos.writeObject((Buyer) user);
-                    oos.flush();
-                }
-                if (command.equals("addToCart")) {
-                    Item selectedItem = (Item) ois.readObject();  // get parameters required from addtocart from user
-                    ((Buyer) user).addItemToCart(selectedItem, objectMapper);
-                    marketplace = server.getUpdatedMarketPlace(objectMapper);
-                    oos.writeObject(marketplace);
-                    oos.flush();
-                }
-                if (command.equals("buyCart")) {
-                    ((Buyer) user).buyCart(marketplace, objectMapper);
-                    marketplace = server.getUpdatedMarketPlace(objectMapper);
-                    user = server.getUpdatedBuyer(user.getUsername(), objectMapper);
-                    oos.writeObject(marketplace);
-                    oos.writeObject((Buyer) user);
-                    oos.flush();
-                }
-                if (command.equals("editUser")) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Accepted connection from " + socket.getInetAddress());
+                ClientThread clientThread = new ClientThread(socket);
+                clientThread.start();
 
-                }
-                if (command.equals("deleteUser")) {
-
-                }
             }
 
-        } catch (ClassNotFoundException e){
-            e.printStackTrace();
-        }
-        catch (IOException e){
+        } catch (IOException e){
             e.printStackTrace();
         }
     }
@@ -95,4 +39,80 @@ public class Server {
     public Buyer getUpdatedBuyer(String username, ObjectMapper objectMapper) throws IOException {
         return JsonUtils.objectByKey(objectMapper, "/buyers/" + username, Buyer.class);
     }
+
+    private static class ClientThread extends Thread {
+        private final Socket clientSocket;
+
+        public ClientThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
+                oos.flush();
+                ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
+
+                // send marketplace to client
+                Marketplace marketplace = JsonUtils.objectByKey(objectMapper, "", Marketplace.class);
+                oos.writeObject(marketplace);
+                oos.flush();
+                System.out.println("Marketplace sent to client!");
+                Person user = (Person) ois.readObject(); // receive person object from client
+                Server server = new Server();
+
+                while (true) {
+                    String command = (String) ois.readObject();
+                    System.out.println("Client Requested " + command);
+                    if (command.equals("buyItem")) {
+                        Item selectedItem = (Item) ois.readObject();  // get parameters required from buyitem from user
+                        String numItems = (String) ois.readObject();  // get parameters required from buyitem from user
+                        ((Buyer) user).buyItem(selectedItem, marketplace, objectMapper, numItems);
+                        marketplace = server.getUpdatedMarketPlace(objectMapper);
+                        oos.writeObject(marketplace);// update client with new marketplace object
+                        oos.flush();
+                    }
+                    if (command.equals("updateMarketplace")) {
+                        marketplace = server.getUpdatedMarketPlace(objectMapper);
+                        oos.writeObject(marketplace);
+                        oos.flush();
+                    }
+                    if (command.equals("updateBuyer")) {
+                        user = server.getUpdatedBuyer(user.getUsername(), objectMapper);
+                        oos.writeObject((Buyer) user);
+                        oos.flush();
+                    }
+                    if (command.equals("addToCart")) {
+                        Item selectedItem = (Item) ois.readObject();  // get parameters required from addtocart from user
+                        ((Buyer) user).addItemToCart(selectedItem, objectMapper);
+                        marketplace = server.getUpdatedMarketPlace(objectMapper);
+                        oos.writeObject(marketplace);
+                        oos.flush();
+                    }
+                    if (command.equals("buyCart")) {
+                        ((Buyer) user).buyCart(marketplace, objectMapper);
+                        marketplace = server.getUpdatedMarketPlace(objectMapper);
+                        user = server.getUpdatedBuyer(user.getUsername(), objectMapper);
+                        oos.writeObject(marketplace);
+                        oos.writeObject((Buyer) user);
+                        oos.flush();
+                    }
+                    if (command.equals("editUser")) {
+
+                    }
+                    if (command.equals("deleteUser")) {
+
+                    }
+                }
+            } catch (ClassNotFoundException e){
+                e.printStackTrace();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
