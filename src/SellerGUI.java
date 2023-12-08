968980c37c440d2673bd007305e0b823a0bdcc2c
vishalkattoju8 <143855658+vishalkattoju8@users.noreply.github.com>
2023-12-08 09:52:55 -0500

    Update SellerGUI.java
    new functionalities

--- src/SellerGUI.java
+++ src/SellerGUI.java
@@ -1,14 +1,19 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class SellerGUI extends JFrame implements ActionListener {
     private Seller seller;
     private ObjectMapper objectMapper;
     private JButton addItemButton;
     private JButton deleteItemButton;
+    private JButton createNewStoreButton;
+    private JButton editStoreButton;
+    private JButton viewStoreSalesButton; // New button for viewing store sales
     private JFrame frame;
 
     public SellerGUI(Seller seller, ObjectMapper objectMapper) {
@@ -24,9 +29,21 @@ public class SellerGUI extends JFrame implements ActionListener {
         deleteItemButton = new JButton("Delete Item from Store");
         deleteItemButton.addActionListener(this);
 
+        createNewStoreButton = new JButton("Create New Store");
+        createNewStoreButton.addActionListener(this);
+
+        editStoreButton = new JButton("Edit Store");
+        editStoreButton.addActionListener(this);
+
+        viewStoreSalesButton = new JButton("View Store Sales"); // Initialize the new button
+        viewStoreSalesButton.addActionListener(this); // Add action listener for the new button
+
         JPanel panel = new JPanel();
         panel.add(addItemButton);
         panel.add(deleteItemButton);
+        panel.add(createNewStoreButton);
+        panel.add(editStoreButton);
+        panel.add(viewStoreSalesButton); // Add the new button to the panel
 
         frame.add(panel);
         frame.setSize(400, 300);
@@ -34,10 +51,20 @@ public class SellerGUI extends JFrame implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
+        // Existing functionality remains the same
         if (e.getSource() == addItemButton) {
             addItemToStore();
         } else if (e.getSource() == deleteItemButton) {
             deleteItemFromStore();
+        } else if (e.getSource() == createNewStoreButton) {
+            createNewStore();
+        } else if (e.getSource() == editStoreButton) {
+            editStore();
+        }
+
+        // Adding functionality for the new button
+        else if (e.getSource() == viewStoreSalesButton) {
+            viewStoreSales(); // Call the method to view store sales
         }
     }
 
@@ -68,6 +95,10 @@ public class SellerGUI extends JFrame implements ActionListener {
         }
     }
 
+
+
+
+
     private void deleteItemFromStore() {
         String storeName = JOptionPane.showInputDialog(frame, "Enter the store name:");
         if (storeName != null && !storeName.isEmpty()) {
@@ -89,8 +120,75 @@ public class SellerGUI extends JFrame implements ActionListener {
         }
     }
 
+    private void createNewStore() {
+        String storeName = JOptionPane.showInputDialog(frame, "Enter the new store name:");
+        if (storeName != null && !storeName.isEmpty()) {
+            // Create a new store object
+            Store newStore = new Store(storeName, new HashMap<>(), new HashMap<>());
+            seller.getStores().put(storeName, newStore); // Add the new store to the seller's stores
+
+            // Perform any additional operations if needed, like updating the database
+
+            JOptionPane.showMessageDialog(frame, "New store created: " + storeName);
+        } else {
+            JOptionPane.showMessageDialog(frame, "Invalid store name!");
+        }
+    }
+
+    private void editStore() {
+        String storeName = JOptionPane.showInputDialog(frame, "Enter the store name to edit:");
+        if (storeName != null && !storeName.isEmpty()) {
+            Store storeToEdit = seller.getStoreByName(storeName);
+            if (storeToEdit != null) {
+                String newStoreName = JOptionPane.showInputDialog(frame, "Enter the new store name:");
+                if (newStoreName != null && !newStoreName.isEmpty()) {
+                    storeToEdit.setName(newStoreName); // Update the store name
+                    seller.getStores().put(newStoreName, storeToEdit); // Update the store in the seller's stores
+
+                    // Perform any additional operations if needed, like updating the database
+
+                    JOptionPane.showMessageDialog(frame, "Store updated: " + newStoreName);
+                } else {
+                    JOptionPane.showMessageDialog(frame, "Invalid store name!");
+                }
+            } else {
+                JOptionPane.showMessageDialog(frame, "Store not found!");
+            }
+        } else {
+            JOptionPane.showMessageDialog(frame, "Invalid store name!");
+        }
+    }
+
+    private void viewStoreSales() {
+        String storeName = JOptionPane.showInputDialog(frame, "Enter the store name to view sales:");
+        if (storeName != null && !storeName.isEmpty()) {
+            Store store = seller.getStoreByName(storeName);
+            if (store != null) {
+                // Assuming you have a method to retrieve sales data for a store
+                String salesData = retrieveSalesDataForStore(store);
+
+                // Display sales data for the selected store
+                JOptionPane.showMessageDialog(frame, "Sales data for " + storeName + ":\n" + salesData);
+            } else {
+                JOptionPane.showMessageDialog(frame, "Store not found!");
+            }
+        } else {
+            JOptionPane.showMessageDialog(frame, "Invalid store name!");
+        }
+    }
+
+    private String retrieveSalesDataForStore(Store store) {
+        // Simulated sales data for demonstration purposes
+        StringBuilder sales = new StringBuilder();
+        sales.append("Total sales: ").append(store.numProductsSold()).append("\n");
+        sales.append("Sold items:\n");
+        for (Map.Entry<String, Item> entry : store.getSoldItems().entrySet()) {
+            sales.append(entry.getValue().getName()).append(" - Quantity: ").append(entry.getValue().getStock()).append("\n");
+        }
+        return sales.toString();
+    }
+
     public static void main(String[] args) {
-        // Assuming you have the necessary objects instantiated (like ObjectMapper and Seller)
         Seller seller = new Seller(); // Replace with your Seller object creation logic
         ObjectMapper objectMapper = new ObjectMapper(); // Replace with your ObjectMapper creation logic
 
